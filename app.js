7ef2f40139c66ed50df0b19e9a4c0659c48d0e61
vivek chamoli <vivekchamoli153@gmail.com>
2026-03-05 15:29:24 +0530

    bootstrap and styling implemented

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const Listing=require("./models/listing");
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderhub";
 const path=require("path");
 const methodOverride=require("method-override");
+const ejsMate=require("ejs-mate");
 
 app.set("view engine",'ejs');
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate); 
+app.use(express.static(path.join(__dirname,"/public"))); 
 
 main().then(()=>{
     console.log("Connected to db");
